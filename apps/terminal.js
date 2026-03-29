2f0f63a4b72e331f6aa9989a0a4da72db718d668
lelbois <164905463+linuxfandudeguy@users.noreply.github.com>
2026-03-29 14:33:18 -0400

    Update terminal.js

--- apps/terminal.js
+++ apps/terminal.js
@@ -3,7 +3,7 @@ const terminalApp = {
     id: "terminal",
     icon: "bi-terminal",
     title: "Terminal (tlick kernal by vvaltchev, v86)",
-    html: `<iframe src="apps/terminal/tlick.html" style="width:100%;height:100%;border:none;"></iframe>`
+    html: `<iframe src="./apps/terminal/index.html" style="width:100%;height:100%;border:none;"></iframe>`
 };
 
 // Export for dynamic loader
